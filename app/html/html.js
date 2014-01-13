24fc1c809afd7d2fd2af6445a2c25f18eaa9c84a
Tingan Ho <tingan87@gmail.com>
2014-01-13 10:32:56 +0800

    Added boilerplate code

---
+++ app/html/html.js
@@ -0,0 +1,19 @@
+
+
+
+
+function Html() {
+
+}
+
+Html.prototype.withBoilerplate = function() {
+
+};
+
+Html.prototype.withLayout = function(name, props) {
+  
+};
+
+Html.prototype.withModule = function(name, props) {
+
+};
