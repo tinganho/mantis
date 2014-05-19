c77f765d88a486985f6f46c8e45795f3e9f42c4a
Tingan Ho <tingan87@gmail.com>
2014-05-19 14:16:40 +0800

    Updated Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,67 +9,68 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
       options: {
-        curly : true,
-        eqeqeq : true,
-        loopfunc : true,
-        forin : false,
-        immed : true,
-        latedef : true,
-        newcap : true,
-        noarg : true,
-        sub : true,
-        undef : true,
-        boss : true,
-        eqnull : true,
-        node : true,
-        es5 : true,
-        supernew : true,
-        laxbreak : true,
-        strict : true,
-        expr : true,
-        laxcomma : true,
+        curly: true,
+        eqeqeq: true,
+        loopfunc: true,
+        forin: false,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        node: true,
+        es5: true,
+        supernew: true,
+        laxbreak: true,
+        strict: true,
+        expr: true,
+        laxcomma: true,
         globals: {
 
           // Native DOM
-          open : true,
-          document : true,
-          navigator : true,
-          confirm : true,
-          performance : true,
-          alert : true,
-          window : true,
-          Image : true,
-          FileReader : true,
-          localStorage : true,
-          sessionStorage : true,
-          cancelAnimationFrame : true,
-          requestAnimationFrame : true,
-          FormData : true,
-          atob : true,
-          Blob : true,
-          Uint8Array : true,
+          open: true,
+          document: true,
+          navigator: true,
+          confirm: true,
+          performance: true,
+          alert: true,
+          window: true,
+          Image: true,
+          FileReader: true,
+          localStorage: true,
+          sessionStorage: true,
+          cancelAnimationFrame: true,
+          requestAnimationFrame: true,
+          FormData: true,
+          atob: true,
+          Blob: true,
+          Uint8Array: true,
 
           // Mocha testing
-          describe : true,
-          it : true,
-          before : true,
+          describe: true,
+          it: true,
+          before: true,
 
           // Libs
-          define : true,
-          requirejs : true,
-          Modernizr : true,
-          mixpanel : true,
+          define: true,
+          requirejs: true,
+          Modernizr: true,
+          mixpanel: true,
 
           // Application
-          App             : true,
-          cf              : true,
-          loginTmpl       : true,
-          streetstyleTmpl : true,
-          tmpl            : true,
-          ENV             : true,
-          terms           : true,
-          JPEG            : true,
-          URL             : true
+          aoo: true,
+          App: true,
+          cf: true,
+          loginTmpl: true,
+          streetstyleTmpl: true,
+          tmpl: true,
+          ENV: true,
+          terms: true,
+          JPEG: true,
+          URL: true
         }
       },
 
@@ -87,130 +88,199 @@ module.exports = function(grunt) {
     },
 
 
-    compass : {
-      documents : {
-        options : {
-          require : compassRequires,
-          sassDir : 'application/documents/styles',
-          cssDir : 'application/public/styles/documents',
-          debugInfo : true,
-          noLineComments : true,
-          imagesDir : 'application/public/images',
-          relativeAssets : true
+    compass: {
+      dev_documents: {
+        options: {
+          config: 'application/configurations/compass.rb',
+          require: compassRequires,
+          sassDir: 'documents/styles',
+          cssDir: 'public/styles/documents',
+          debugInfo: true,
+          noLineComments: true,
+          imagesDir: 'application/public/images'
         }
       },
 
-      content : {
-        options : {
-          require : compassRequires,
-          sassDir : 'application/content/styles',
-          cssDir : 'application/public/styles/content',
-          debugInfo : true,
-          noLineComments : true,
-          imagesDir : 'application/public/images',
-          relativeAssets : true
+      dev_contents: {
+        options: {
+          config: 'application/configurations/compass.rb',
+          require: compassRequires,
+          sassDir: 'contents/styles',
+          cssDir: 'public/styles/contents',
+          debugInfo: true,
+          noLineComments: true,
+          imagesDir: 'public/images'
+        }
+      },
+
+      dist_documents: {
+        options: {
+          config: 'application/configurations/compass.rb',
+          require: compassRequires,
+          sassDir: 'documents/styles',
+          cssDir: 'public/styles/documents',
+          debugInfo: true,
+          noLineComments: true,
+          imagesDir: 'public/images',
+          outputStyle: 'compressed'
+        }
+      },
+
+      dist_contents: {
+        options: {
+          config: 'application/configurations/compass.rb',
+          require: compassRequires,
+          sassDir: 'contents/styles',
+          cssDir: 'public/styles/contents',
+          debugInfo: true,
+          noLineComments: true,
+          imagesDir: 'public/images',
+          outputStyle: 'compressed'
         }
       }
     },
 
     watch: {
-
-      jshint : {
+      jshint: {
         files: [
-          '**/*.js'
+          'application/contents/**/*.js',
+          'application/configurations/**/*.js',
+          'application/core/**/*.js',
+          'application/libraries/**/*.js'
         ],
         tasks: ['jshint']
       },
 
-      compass: {
-        files: ['application/**/*.scss'],
-        tasks: ['compass:dev']
+      templates : {
+        files: [
+          'application/contents/**/*.{part,dot}',
+          'application/layouts/**/*.{part,dot}',
+          'application/documents/**/*.{part,dot}'
+        ],
+        tasks: ['dot']
+      },
+
+      compass_documents: {
+        files: [
+          'application/documents/**/*.scss',
+          'application/layouts/**/*.scss',
+          'application/components/**/*.scss'
+        ],
+        tasks: ['compass:dev_documents']
+      },
+
+      compass_contents: {
+        files: ['application/contents/**/*.scss'],
+        tasks: ['compass:dev_contents']
       }
     },
 
     clean: {
       app: [
-        'dist',
+        'distribution',
         'application/public/styles/*',
-        'application/public/scripts/*',
+        'application/public/scripts/configurations/*',
+        'application/public/scripts/distribution/*',
         'application/public/images-webp/*',
         'application/public/tmp/*',
         'application/public/uploads/*',
         'application/public/templates/*'
       ],
-      dist: [
-        'dist/vendor/**/*',
-        '!dist/vendor/requirejs/*.require.js',
-        '!dist/vendor/modernizr/*.modernizr.js',
-        'dist/components'
-      ],
       deploy: ['deploy'],
       css: ['application/public/styles/*']
     },
 
-    rev: {
-      options: {
-        algorithm: 'sha1',
-        length: 8
-      },
-      files: {
-        src: [
-          'dist/public/styles/**/*.css',
-          'dist/public/conf/**/*.js',
-          'dist/public/scripts/**/*.js',
-          'dist/vendor/modernizr/modernizr.js',
-          'dist/vendor/requirejs/require.js',
-          'dist/modules/device/features.js',
-          'dist/public/images/**/*.{png,jpg}',
-          '!dist/public/images/main/**/*',
-          '!dist/public/images/main2x/**/*',
-          '!dist/public/images/flags/**/*',
-          '!dist/public/images/flags2x/**/*',
-          'dist/public/images-webp/**/*.webp',
+    copy: {
+      dist: {
+        files: [
+          { expand: true, cwd: 'application/', src: ['**'], dest: 'distribution/' }
         ]
       }
     },
 
-    dot : {
-      core : {
-        options : {
-          variable : 'tmpl',
-          requirejs : true,
-          node : true
+    dot: {
+      dev_core: {
+        options: {
+          variable: 'tmpl',
+          requirejs: true,
+          node: true
         },
-        src : ['application/core/**/*.dot'],
-        dest : 'application/public/templates/core/templates.js'
+        src: ['application/core/**/*.dot'],
+        dest: 'application/public/templates/core/templates.js'
       },
 
-      documents : {
-        options : {
-          variable : 'tmpl',
-          requirejs : true,
-          node : true
+      dev_documents: {
+        options: {
+          variable: 'tmpl',
+          requirejs: true,
+          node: true
         },
-        src : ['application/documents/**/*.dot'],
-        dest : 'application/public/templates/documents/templates.js'
+        src: ['application/documents/**/*.dot'],
+        dest: 'application/public/templates/documents/templates.js'
       },
 
-      layouts : {
-        options : {
-          variable : 'tmpl',
-          requirejs : true,
-          node : true
+      dev_layouts: {
+        options: {
+          variable: 'tmpl',
+          requirejs: true,
+          node: true
         },
-        src : ['application/layouts/**/*.dot'],
-        dest : 'application/public/templates/layouts/templates.js'
+        src: ['application/layouts/**/*.dot'],
+        dest: 'application/public/templates/layouts/templates.js'
       },
 
-      content_app : {
-        options : {
-          variable : 'tmpl',
-          requirejs : true,
-          node : true
+      dev_content_app: {
+        options: {
+          variable: 'tmpl',
+          requirejs: true,
+          node: true
         },
-        src : [
+        src: [
+          'application/contents/share/**/*.dot'
         ],
-        dest : 'application/public/templates/content/templates.js'
+        dest: 'application/public/templates/content/templates.js'
+      },
+
+      dist_core: {
+        options: {
+          variable: 'tmpl',
+          requirejs: true,
+          node: true
+        },
+        src: ['distribution/core/**/*.dot'],
+        dest: 'distribution/public/templates/core/templates.js'
+      },
+
+      dist_documents: {
+        options: {
+          variable: 'tmpl',
+          requirejs: true,
+          node: true
+        },
+        src: ['distribution/documents/**/*.dot'],
+        dest: 'distribution/public/templates/documents/templates.js'
+      },
+
+      dist_layouts: {
+        options: {
+          variable: 'tmpl',
+          requirejs: true,
+          node: true
+        },
+        src: ['distribution/layouts/**/*.dot'],
+        dest: 'distribution/public/templates/layouts/templates.js'
+      },
+
+      dist_content_app: {
+        options: {
+          variable: 'tmpl',
+          requirejs: true,
+          node: true
+        },
+        src: [
+          'distribution/contents/share/**/*.dot'
+        ],
+        dest: 'distribution/public/templates/content/templates.js'
       }
     },
 
@@ -224,96 +294,128 @@ module.exports = function(grunt) {
       },
     },
 
-    translate: {
-      dist: {
-        options: {
-          config          : './application/translations',
-          requirejs       : true,
-          defaultLanguage : 'en', // grunt-translate use it to update translation.
-          output          : './application/translations/output',
-          src             : ['./application/**/*.js', '!./application/vendor/**/*.js'],
-          interface: {
-            autoOpen : false,
-            port     : 3001
-          }
-        }
-      }
-    },
-
-    useminPrepare: {
-      html: 'application/profiles/p1/lib/templates/layout/login-shell.dot',
+    rev: {
       options: {
-        dest: 'dist'
+        algorithm: 'sha1',
+        length: 8
+      },
+      files: {
+        src: [
+          'distribution/public/styles/**/*.css',
+          'distribution/public/scripts/**/*.js',
+          'distribution/vendor/modernizr/modernizr.js',
+          'distribution/vendor/requirejs/require.js',
+          'distribution/public/images/**/*.{png,jpg}',
+          'distribution/public/images-webp/**/*.webp',
+        ]
       }
     },
 
     usemin: {
-      html: ['dist/**/*.dot', 'dist/**/*.part'],
-      css: ['dist/**/*.css', '!dist/vendor/**'],
+      html: ['distribution/**/*.dot', 'distribution/**/*.part'],
+      css: ['distribution/public/**/*.css', '!distribution/vendor/**'],
       options: {
-        root : 'dist',
-        basedir : 'dist',
-        dirs: ['dist']
-      }
-    },
-
-    copy: {
-      main: {
-        files: [
-          {expand: true, cwd: 'application/', src: ['**'], dest: 'dist/'}, // makes all src relative to cwd
-        ]
+        root : 'distribution',
+        basedir : 'distribution',
+        dirs: ['distribution']
       }
     },
 
     uglify: {
       dist: {
         files: {
-          'dist/vendor/requirejs/require.js'   : ['dist/vendor/requirejs/require.js'],
-          'dist/vendor/modernizr/modernizr.js' : ['dist/vendor/modernizr/modernizr.js'],
-          'dist/translations/output/en.js'     : ['dist/translations/output/en.js'],
-          'dist/translations/output/zh.js'     : ['dist/translations/output/zh.js']
+          'distribution/vendor/requirejs/require.js': ['distribution/vendor/requirejs/require.js'],
+          'distribution/vendor/modernizr/modernizr.js': ['distribution/vendor/modernizr/modernizr.js']
+        }
+      }
+    },
+
+    requirejs: {
+      application: {
+        options: {
+          optimize: 'uglify',
+          preserveLicenseComments : false,
+          // generateSourceMaps : true,
+          baseUrl: 'application',
+          mainConfigFile: 'application/documents/mains/application.js',
+          out: 'application/public/scripts/distribution/application.js',
+          name: 'documents/mains/application',
+          paths: {
+            'modernizr': 'empty:'
+          }
         }
       }
     },
 
     webp: {
-      dev : {
+      dev: {
         expand: true,
-        cwd : 'application/public/images',
-        src : [
-          '*.{jpg,png}',
-          'backgrounds/*.{jpg,png}'
-        ],
-        dest : 'application/public/images-webp'
-      },
-      dist : {
-        expand: true,
-        cwd: 'dist/public/images',
+        cwd: 'application/public/images',
         src: [
           '*.{jpg,png}',
           'backgrounds/*.{jpg,png}'
         ],
-        dest: 'dist/public/images-webp'
+        dest: 'application/public/images-webp'
+      },
+      dist: {
+        expand: true,
+        cwd: 'distribution/public/images',
+        src: [
+          '*.{jpg,png}',
+          'backgrounds/*.{jpg,png}'
+        ],
+        dest: 'distribution/public/images-webp'
       },
       options: {
         lossless: true,
       }
     },
 
-    requirejs: {
+    modernizr: {
+      dist: {
+        'devFile': 'distribution/vendor/modernizr/modernizr.js',
+        'outputFile': 'distribution/vendor/modernizr/modernizr.js',
+
+        // Based on default settings on http://modernizr.com/download/
+        'extra' : {
+          'shiv' : true,
+          'printshiv' : false,
+          'load' : true,
+          'mq' : false,
+          'cssclasses' : true
+        },
+
+        // Based on default settings on http://modernizr.com/download/
+        'extensibility' : {
+          'addtest' : false
+        },
+
+        // By default, source is uglified before saving
+        'uglify' : true,
+
+        // Define any tests you want to implicitly include.
+        'tests' : [
+          'csstransforms'
+        ],
+
+        // When parseFiles = true, matchCommunityTests = true will attempt to
+        // match user-contributed tests.
+        'matchCommunityTests' : false
+      }
     }
-
-
   });
 
   // Build task
   grunt.registerTask('build', [
     'clean:app',
-    'compass:dist',
-    'dot:devLogin',
-    'dot:devStreetstyle',
+    'compass:dist_documents',
+    'compass:dist_contents',
+    'dot:dev_core',
+    'dot:dev_documents',
+    'dot:dev_layouts',
+    'dot:dev_content_app',
     'requirejs',
-    'copy:main',
+    'copy:dist',
     'webp:dist',
     'uglify:dist',
     'rev',
@@ -321,8 +423,10 @@ module.exports = function(grunt) {
 
     // // Because the dot templates has changed
     // // we need to run the process again
-    'dot:distLogin',
-    'dot:distStreetstyle'
+    'dot:dist_core',
+    'dot:dist_documents',
+    'dot:dist_layouts',
+    'dot:dist_content_app',
   ]);
 
   grunt.registerTask('deploy', [
@@ -353,5 +457,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-zip');
   grunt.loadNpmTasks('grunt-webp');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks("grunt-modernizr");
 
 };
